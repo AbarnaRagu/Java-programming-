7f6370fc1060560592497d424c5ff9d4ebc127fa
AbarnaRagu <131658876+AbarnaRagu@users.noreply.github.com>
2023-11-13 23:30:58 +0530

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class method {
+    public static void main(String args[])
+    {
+        String s1="Abarna";
+        String s3=" ";
+        System.out.println(s1);
+        System.out.println(s1.toUpperCase());
+        String s2=s1.toLowerCase();
+        System.out.println(s2);
+        System.out.println(s1.length());
+        System.out.println(s1.indexOf('A'));
+        System.out.println(s1.indexOf('n'));
+        System.out.println(s1.lastIndexOf('a'));
+        System.out.println(s3.isEmpty());
+        System.out.println(s3.isBlank());
+    }
+}
