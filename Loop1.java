6e516fee7ab81a2ecc8d418eef9c13a533633134
AbarnaRagu <131658876+AbarnaRagu@users.noreply.github.com>
2023-12-20 23:41:19 +0530

    Create Loop1.java

---
+++ Loop1.java
@@ -0,0 +1,14 @@
+public class loop
+{
+    public static void main(String[] args)
+    {
+    int i=1;
+    do{
+    System.out.println("hello"+i);
+    i++;
+    }
+    while(i<=3);
+     System.out.println(i);
+
+    }
+}
